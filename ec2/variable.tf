ee5a9b4565d060db16a0d54b5485e42f6071fe76
Marcus Liu <me@mail.lmarcus.onl>
2020-11-05 02:25:21 +0800

    [Marcus] 指定 ec2 在哪個 az

---
+++ ec2/variable.tf
@@ -0,0 +1,10 @@
+variable "master_availability_zone" {
+    type = string
+    default = "ap-northeast-1a"
+}
+
+variable "slave_availability_zone" {
+    type = string
+    default = "ap-northeast-1d"
+}
+
