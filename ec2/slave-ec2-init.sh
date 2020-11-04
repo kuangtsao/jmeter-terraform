d963e12f7bd9758362910fd431a894ad0f105bb0
Marcus Liu <me@mail.lmarcus.onl>
2020-11-05 02:23:34 +0800

    [Marcus] 移動檔案

--- ec2/slave-ec2-init.sh
+++ ec2/slave-ec2-init.sh
@@ -8,4 +8,4 @@ sudo add-apt-repository \
     stable"
 sudo apt install -y docker-ce docker-ce-cli containerd.io
 sudo systemctl enable docker.service
-sudo docker run -d -e LOCALIP=$instance_ip --name jmeter-slave -p 1099:1099 -p 50000:50000 vinsdocker/jmawsserver /bin/bash
+sudo docker run -d -e LOCALIP --name jmeter-slave -p 1099:1099 -p 50000:50000 vinsdocker/jmawsserver /bin/bash
