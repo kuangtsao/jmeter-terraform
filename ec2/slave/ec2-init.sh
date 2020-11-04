f6d59caeed860de5b0b5fb76253e83e5396a4c5b
Marcus Liu <me@mail.lmarcus.onl>
2020-11-04 17:26:23 +0800

    [Marcus] 完成 jmeter-slave 機所需的前置安裝 shell

---
+++ ec2/slave/ec2-init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+instance_ip=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+sudo apt update
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo systemctl enable docker.service
+sudo docker run -d \
+    -e LOCALIP=$instance_ip \ 
+    --name jmeter-slave \
+    -p 1099:1099 \
+    -p 50000:50000 \
+    vinsdocker/jmawsserver /bin/bash
